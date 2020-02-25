9a327ccb032b67d608a5e991844f03fc1e0fd3d1
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-02-25 22:10:24 +0800

    edited user.py & added fields

--- models/user.py
+++ models/user.py
@@ -3,4 +3,15 @@ import peewee as pw
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    usernname = pw.CharField(unique=True)
+    email = pw.CharField(unique=True)
+    password = pw.CharField(null=False)
+
+    def validate(self):
+        duplicate_user = User.get_or_none(User.usernname == self.username)
+        duplicate_email = User.get_or_none(User.email == self.email)
+
+        if duplicate_user:
+            self.errors.append("That name is taken.")
+        elif duplicate_email:
+            sels.errors.append("An account with that email already exists.")
