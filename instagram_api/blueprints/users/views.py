e56c3bc2a79b4acf573e9496203b7810d723ce84
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-03-06 11:09:50 +0800

    amended buildpack

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,6 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask_jwt import JWT, jwt_required
 from models.user import User
+from playhouse.shortcuts import model_to_dict
+
 
 users_api_blueprint = Blueprint('users_api',
                                 __name__,
@@ -9,30 +11,38 @@ users_api_blueprint = Blueprint('users_api',
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
-    return "USERS API"
+    users = User.select()
+
+    user_data = []
+
+    for user in users:
+        user = model_to_dict(user)
+        user_data.append(user)
+
+    return jsonify(user_data), 200
 
 
-@users_api_blueprint("/<username>", methods=["POST"])
-@jwt_required
-def show(username):
-    user = User.get_or_none(User.username == username)
+# @users_api_blueprint("/<username>", methods=["POST"])
+# @jwt_required
+# def show(username):
+    # user = User.get_or_none(User.username == username)
 
-    if not user:
-        resp = {
-            "message": "No user found with that username!",
-            "ok": False
-        }
+    # if not user:
+    #     resp = {
+    #         "message": "No user found with that username!",
+    #         "ok": False
+    #     }
 
-        return jsonify(resp)
+    #     return jsonify(resp)
 
-    resp = {
-        "message": "Found user with that username",
-        "user": {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email
-        },
-        "ok": True
-    }
-    return jsonify(resp)
-    pass
+    # resp = {
+    #     "message": "Found user with that username",
+    #     "user": {
+    #         "id": user.id,
+    #         "username": user.username,
+    #         "email": user.email
+    #     },
+    #     "ok": True
+    # }
+    # return jsonify(resp)
+    # pass
